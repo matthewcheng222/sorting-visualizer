3197f3c3b2b161df1a88466ffe0586e5c531fee5
Matthew Cheng <matthewcheng222@gmail.com>
2025-03-26 22:51:30 +0000

    Changed base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sorting-visualizer/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
